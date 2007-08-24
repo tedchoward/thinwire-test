60fda72c1f3012c3f2a7e985105775cdb7424e1b
Joshua Gertzen <me@joshgertzen.com>
2007-08-24 07:34:28 +0000

    * Cleaned up the license headers

--- src/thinwire/apps/test/TabFolderTest.java
+++ src/thinwire/apps/test/TabFolderTest.java
@@ -1,4 +1,7 @@
 /*
+                          ThinWire(R) Framework Tests
+                 Copyright (C) 2006-2007 Custom Credit Systems
+
 	This library is free software; you can redistribute it and/or modify it under
 	the terms of the GNU Lesser General Public License as published by the Free
 	Software Foundation; either version 2.1 of the License, or (at your option) any
