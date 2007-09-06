ad367efdf0033a70c6fed79b5bbff527ac4aca79
Joshua Gertzen <me@joshgertzen.com>
2007-09-06 19:57:19 +0000

    * Added test that recreates bug #1679389 "Menu: Incorrect double highlight behavior on long menu items"

---
+++ src/thinwire/apps/test/MenuHighlightBugTest.java
@@ -0,0 +1,42 @@
+/*
+                          ThinWire(R) Framework Tests
+                 Copyright (C) 2006-2007 Custom Credit Systems
+
+	This library is free software; you can redistribute it and/or modify it under
+	the terms of the GNU Lesser General Public License as published by the Free
+	Software Foundation; either version 2.1 of the License, or (at your option) any
+	later version.
+
+	This library is distributed in the hope that it will be useful, but WITHOUT ANY
+	WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+	PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License along
+	with this library; if not, write to the Free Software Foundation, Inc., 59
+	Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+package thinwire.apps.test;
+
+import thinwire.ui.*;
+
+//Recreates a bug from RC1 regarding menu highlighting.
+//Tested against r462 and the bug still occured.
+public class MenuHighlightBugTest {
+	public static void main(String[] args) {
+		Frame f = Application.current().getFrame();
+		Menu menu = new Menu();
+		f.setMenu(menu);
+		Menu.Item root = menu.getRootItem();
+		
+		Menu.Item help = new Menu.Item("&Highlight");
+		root.getChildren().add(help);
+		
+		for (int i = 0; i < 15; i++) {
+			String text = "Item " + i;
+			//if (i == 0) text = ""; //Works Fine
+			if (i == 1) text = ""; //Causes highlight issue
+			//if (i == 2) text = ""; //Works Fine
+			help.getChildren().add(new Menu.Item(text));
+		}
+	}
+}
